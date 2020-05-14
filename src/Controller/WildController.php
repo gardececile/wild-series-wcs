cc0dd65b4da4b614d9622d6e0f042732e0f34cf1
cecileg67380 <gardececile@gmail.com>
2020-05-14 16:03:17 +0200

    contenu et challenge  route slug methode=show  show.html.twig

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -3,20 +3,42 @@
 
 namespace App\Controller;
 
+//use Psr\Container\ContainerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+/**
+ * @Route("/wild", name="wild_")
+ */
 
 class WildController extends AbstractController
 {
     /**
-     * @Route("/wild", name="wild_index")
+     * @Route("/", name="index")
      * @return Response
      */
     public function index() : Response
     {
-        return $this->render('wild/index.html.twig', [
-            'website' => 'Wild Séries',
+        return $this->render('/index.html.twig',
+            ['website' => 'Wild Séries',
         ]);
     }
+
+    /**
+     * @Route("/show/{slug}",
+     *     requirements={"slug"="[a-z0-9\-]+"},
+     *     defaults={"slug"="Aucune série sélectionnée, veuillez choisir une série"},
+     *     name="show"
+     * )
+     */
+    public function show ($slug): Response
+    {
+        //remplacer tous les tirets du slug par des espaces
+        $adaptedSlug=str_replace('-',' ',$slug);
+            // puis passer la première lettre de chaque mot en MAJUSCULE
+        $UpAdaptedSlug=ucwords($adaptedSlug);
+        return $this->render('wild/show.html.twig',
+            ['slug'=>$UpAdaptedSlug]);
+    }
+
 }
