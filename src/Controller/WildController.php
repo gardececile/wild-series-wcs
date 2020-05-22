ffe9b4429e8f2ccc6b65e87dbb9486506283ce17
cecileg67380 <gardececile@gmail.com>
2020-05-22 14:27:54 +0200

    quete09SymfonyLesRelationsBidirectionnellesavecDoctrine : méthode showByProgram() fonctionne avec création de la page program.html.twig pour le rendre visible

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 
 //use Psr\Container\ContainerInterface;
 use App\Entity\Category;
+use App\Entity\Season;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -92,6 +93,45 @@ class WildController extends AbstractController
         return $this->render('wild/category.html.twig', [
             'programs' => $program,
     ]);
+    }
+
+    /**
+     * Getting a program to show depending of a formatted slug for title
+     * la méthode showByProgram() récupère un programme à partir d'un slug passé dans l'url
+     *
+     * @param string $slug The slugger
+     * @Route("/program/{slug<^[a-z0-9-]+$>}", defaults={"slug" =null}, name="show_program")
+     *
+     **/
+    public function showByProgram(?string $slug): Response
+    {
+        if (!$slug) {
+            throw $this
+                ->createNotFoundException('No slug has been sent to find a program.');
+        }
+        $slug = preg_replace(
+            '/-/',
+            ' ', ucwords(trim(strip_tags($slug)), "-")
+        );
+        $program = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findOneBy(['title'=> mb_strtolower($slug)]);
+
+        $seasons= $this->getDoctrine()
+            ->getRepository(Season::class)
+            ->findBy(['program'=>$program]);
+
+        if (!$program) {
+            throw $this->createNotFoundException(
+                'No program with' .$slug.'title, found in program\'s table.'
+            );
+        }
+
+        return $this->render('wild/program.html.twig', [
+            'program' => $program,
+            'slug' => $slug,
+            'seasons' =>$seasons,
+        ]);
     }
 
 }
