e96ac4b50cd8c5287571a7431cd7b82a8e5c63b8
cecileg67380 <gardececile@gmail.com>
2020-06-04 23:29:26 +0200

    quete11 Formulaire ProgramSearchType.php à jour buildForm (déjà sauvegardé) et surtout la méthode configureOptions ajoutée

--- src/Form/ProgramSearchType.php
+++ src/Form/ProgramSearchType.php
@@ -5,10 +5,19 @@ namespace App\Form;
 
 
 
+use App\Entity\Category;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ProgramSearchType extends \Symfony\Component\Form\AbstractType
 {
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Category::class,
+        ]);
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder->add('searchField');
