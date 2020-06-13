878fa55a051ad0d8b3034631da45ec2a05a60565
cecileg67380 <gardececile@gmail.com>
2020-06-13 17:01:09 +0200

    modif page d accueil home.html.twig generale

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProgramRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,8 +14,10 @@ class DefaultController extends AbstractController
      * @Route ("/", name="app_index")
      * @return Response
      */
-    public function index() : Response
+    public function index(ProgramRepository $programRepository) : Response
     {
-        return $this->render('/home.html.twig');
+        return $this->render('/home.html.twig', [
+            'programs'=>$programRepository->findAll()]
+        );
     }
 }
